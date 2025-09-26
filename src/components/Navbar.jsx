2ad9e1ab9d1a20ad5134a9933c1a7633a2960922
sadhin28 <taosifsadhin113@gmail.com>
2025-09-26 10:36:31 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white px-6 py-3 flex justify-between items-center">
+      {/* Logo */}
+      <Link to="/" className="text-xl font-bold">
+        ফ্রি রিওয়ার্ড
+      </Link>
+
+      {/* Menu */}
+      <ul className="flex space-x-6">
+        <li>
+          <Link to="/" className="hover:text-gray-200">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/offers" className="hover:text-gray-200">
+            Offers
+          </Link>
+        </li>
+        <li>
+          <Link to="/dashboard" className="hover:text-gray-200">
+            Dashboard
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
