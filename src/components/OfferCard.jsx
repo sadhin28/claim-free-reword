870f073996216d749cb8dad2ce32e8ad3afbfadf
sadhin28 <taosifsadhin113@gmail.com>
2025-09-27 20:23:20 +0600

    add link

--- src/components/OfferCard.jsx
+++ src/components/OfferCard.jsx
@@ -24,9 +24,9 @@ export default function OfferCard({ data }) {
 
       {/* Claim / CTA Button */}
       <a
-        href="https://smrturl.co/a/s4ea50d515f/15266?s1=free-reward-hub"
+        href="https://smrturl.co/a/s4ea50d515f/1898?s1="
         target="_blank"
-        rel="noopener noreferrer"
+        // rel="noopener noreferrer"
         className="mt-4 block w-full text-center bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
       >
         Claim Reward
