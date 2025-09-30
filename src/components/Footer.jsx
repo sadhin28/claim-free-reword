75ea11206dc62b1bf254212e39bb77498c9c5cab
sadhin28 <taosifsadhin113@gmail.com>
2025-09-30 17:51:33 +0600

    add purple footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="border-t-2 bg-gradient-to-r from-indigo-400 via-blue-400 to-purple-700 text-white px-6  text-center py-4">
+    <footer className="border-t-2 bg-gradient-to-r from-indigo-400 via-blue-400 to-purple-400 text-white px-6  text-center py-4">
       <p>
       © {year} Free Reward Hub .Com
       </p>
